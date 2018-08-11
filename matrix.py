398e4b217f908abf1119afeca286a08769d0aef7
inxoy <i15098@student.htlwrn.ac.at>
2018-08-11 22:11:57 +0200

    Add create_matrix

--- matrix.py
+++ matrix.py
@@ -6,3 +6,11 @@ def is_matrix(matrix):
     for i in range(1, len(matrix)):
         if len(matrix[i-1]) != len(matrix[i]):
             raise Exception("Rows must have the same length")
+
+def create_matrix(rows, cols, creation):
+    matrix = [[0 for i in range(cols)] for j in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            matrix[i][j] = creation(i, j)
+    return matrix
