ab931e724ab64f206160dec036dec67cfb585078
inxoy <i15098@student.htlwrn.ac.at>
2018-08-11 17:13:21 +0200

    Add sub

--- vector.py
+++ vector.py
@@ -1,20 +1,29 @@
 #A vector e.g. [1, 2 ,3]  is defined as a lsit of values where 1 is the x component, 2 the y component...
 
-def add(vectors):
-    added_vector = vectors[0]
+def concat(vectors, combine):
+    combined_vector = vectors[0]
 
     for i in range(0, len(vectors)):
         if i > 0:
             if len(vectors[i-1]) != len(vectors[i]):
-                raise Exception('Vectors must have the same dimensions!')
+                raise Exception('Vectors must be the same dimensions!')
 
             for j in range(0, len(vectors[0])):
-                added_vector[j] += vectors[i][j]
+                combined_vector[j] = combine(combined_vector[j], vectors[i][j])
 
-    return added_vector
+    return combined_vector
 
-a = [1,2,3]
+def add(vectors):
+    return concat(vectors, lambda a, b: a + b)
+
+def sub(vectors):
+    return concat(vectors, lambda a, b: a - b)
+
+
+
+
+a = [1,2,2]
 b = [2,3,4]
 c = [1,2,3]
 
-print(add([a,b,c]))
+print(sub([a,b,c]))
