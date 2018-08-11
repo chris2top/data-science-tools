2c8776c45fa8004f01aa12f8640f866781fc0c91
inxoy <i15098@student.htlwrn.ac.at>
2018-08-11 22:08:10 +0200

    Add is_matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,8 @@
 """A matrix e.g. [[1,2,3], is defined as a list of lists where the inner lists represent the rows.
                   [4,5,6]]  e.g. the first element in every row translate to the first column
 """
+
+def is_matrix(matrix):
+    for i in range(1, len(matrix)):
+        if len(matrix[i-1]) != len(matrix[i]):
+            raise Exception("Rows must have the same length")
