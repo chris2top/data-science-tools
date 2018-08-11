15afa5d7efe1b535abf2ea3ba0b64cf00e96b80e
inxoy <i15098@student.htlwrn.ac.at>
2018-08-11 21:08:46 +0200

    Add matrix.py

---
+++ matrix.py
@@ -0,0 +1,3 @@
+"""A matrix e.g. [[1,2,3], is defined as a list of lists where the inner lists represent the rows.
+                  [4,5,6]]  e.g. the first element in every row translate to the first column
+"""
