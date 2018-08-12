581c567638cda5a0828aa391d52b81d30008520d
inxoy <i15098@student.htlwrn.ac.at>
2018-08-12 08:38:45 +0200

    Add mode

--- statistics.py
+++ statistics.py
@@ -10,6 +10,32 @@ def median(list):
 
     return mean([list[median-1], list[median]])
 
-list = [1,2,4,5,6]
+def q(list, p):
+    q = int(len(list) * p)
+    return sorted(list)[q]
+
+def mode(list):
+    list = sorted(list)
+    count = 0
+    max_count = (0, 0)
+    prev_element = list[0]
+
+    for element in list:
+        if element == prev_element:
+            count += 1
+        else:
+            count = 0
+
+        if (count >= max_count[1]):
+            max_count = (element, count)
+        prev_element = element
+        print(max_count)
+
+    return max_count[0]
+
+
+list = [1,2,2,3,3,4,4,5,5,5,6]
 
 print(median(list))
+
+print(mode(list))
