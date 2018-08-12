d4bed3fc82da34b0a20d33849674fe697660b2f7
inxoy <i15098@student.htlwrn.ac.at>
2018-08-12 09:16:50 +0200

    Add standard deviation, covariance and refactor variance

--- statistics.py
+++ statistics.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from vector import dot_product
 
 def mean(list):
     return sum(list) / len(list)
@@ -42,17 +43,22 @@ def mode(list):
 def scattering(list):
     return max(list) - min(list)
 
-def variance(list, n=None): #sometimes counting starts at 0, which results in the variance to be divided by n-1
-    if n == None:
-        n = len(list)
-
+def variance(list, offset=0): #sometimes counting starts at 0, which results in the variance to be divided by n-1
+    n = len(list)
     mean1 = mean(list)
 
-    return sum([(mean1 - element) ** 2 / len(list) for element in list])
+    return sum([(mean1 - element) ** 2 / (n - offset) for element in list])
 
-def standard_deviation(list, n=None):
+def standard_deviation(list, n=0):
     return sqrt(variance(list, n))
 
+def covariance(list_a, list_b, offset=0):  #sometimes counting starts at 0, which results in the covariance to be divided by n-1
+    if (len(list_a) != len(list_b)):
+        raise Exception("lists must be same length")
+
+    n = len(list_a)
+    return dot_product([sub_mean(list_a), sub_mean(list_b)]) / n - offset
+    
 list = [8,7,9,10,6]
 
 print(mean(sub_mean(list)))
@@ -61,3 +67,5 @@ print(median(list))
 print(mode(list))
 
 print(standard_deviation(list))
+
+print(covariance([100,49,41,40,25],[22,10,10,10,5]))
