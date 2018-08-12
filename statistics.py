5111d13442ac2b789924037d42d19c967167381c
inxoy <i15098@student.htlwrn.ac.at>
2018-08-12 08:49:27 +0200

    Add scattering, variance and standard deviation

--- statistics.py
+++ statistics.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def mean(list):
     return sum(list) / len(list)
 
@@ -14,6 +16,7 @@ def q(list, p):
     q = int(len(list) * p)
     return sorted(list)[q]
 
+#TODO more than one mode could exist
 def mode(list):
     list = sorted(list)
     count = 0
@@ -29,13 +32,27 @@ def mode(list):
         if (count >= max_count[1]):
             max_count = (element, count)
         prev_element = element
-        print(max_count)
 
     return max_count[0]
 
+def scattering(list):
+    return max(list) - min(list)
 
-list = [1,2,2,3,3,4,4,5,5,5,6]
+def variance(list, n=None): #sometimes counting starts at 0, which results in the variance to be divided by n-1
+    if n == None:
+        n = len(list)
+
+    mean1 = mean(list)
+
+    return sum([(mean1 - element) ** 2 / len(list) for element in list])
+
+def standard_deviation(list, n=None):
+    return sqrt(variance(list, n))
+
+list = [8,7,9,10,6]
 
 print(median(list))
 
 print(mode(list))
+
+print(standard_deviation(list))
